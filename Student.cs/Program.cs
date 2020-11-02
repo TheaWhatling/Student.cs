fa8e67d21bca92f1955a1b4b9ca25de5d7fc28fe
Thea Whatling <K038218@eltham-college.org.uk>
2020-11-02 11:29:56 +0000

    Add project files.

---
+++ Student.cs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Student.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student v, v1;                   // classes are always capital letters
+            v = new Student(bill, 110);
+            //v.getName();
+            //Console.WriteLine(v);
+
+            v1 = new Student();
+            //v1.getName();
+            //Console.WriteLine(v1);
+
+            v.setName("bill");
+            v.setName("ben");
+
+            Console.WriteLine(v.getName());
+
+            for(int x = 0; x < 10; x = x + 1)
+            {
+                v.learn(1);
+            }
+
+            Console.WriteLine(v.getName() + "  " + v.predictedGrade());
+        }
+    }
+}
---
+++ Student.cs/Student.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Student.cs
+{
+    class Student // "static" is not used for objects in object orientated code but is for classes in other type of code 
+    {
+        private string name;
+        private int IQ;
+
+        public Student()
+        {
+            name = "bob";
+            IQ = 100;
+        }
+
+        public Student(string name)
+        {
+            this.name = name;
+            this.IQ = -100; 
+        }
+
+        public Student(string name, int IQ)
+        {
+            this.name = name;
+            this.IQ = IQ;
+        }
+
+        public void gaming(int v)
+        {
+            IQ = IQ - v;
+        }
+
+        public void learn(int hours)
+        {
+            IQ = IQ + hours;
+        }
+
+        public void predictedGrade()
+        {
+            string p;
+            p = " ";
+
+            if (IQ > 150)
+            {
+                p = " A* ";
+            }
+            else p = "A";
+
+        }
+
+
+        public void setName(string n)
+        {
+            name = n;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+    }
+
+}
